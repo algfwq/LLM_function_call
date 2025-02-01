0751cd07c08d011d89611ca1648982f730ed05f4
algfwq <3104374883@qq.com>
2025-02-01 23:26:00 +0800

    :sparkles: 搜索工具（但是：失败）

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from langchain_openai import ChatOpenAI
 from pydantic import SecretStr
 from langchain_core.tools import tool
 from langchain_core.messages import HumanMessage
+from langchain_community.tools.tavily_search import TavilySearchResults
+import os
+
+# 设置tavily搜索工具的API Key
+os.environ["TAVILY_API_KEY"] = "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"
 
 # 基础模型
 llm = ChatOpenAI(
@@ -9,6 +14,12 @@ llm = ChatOpenAI(
     base_url="http://127.0.0.1:1234/v1",
     api_key=SecretStr("lm-studio"),
 )
+
+# llm = ChatOpenAI(
+#     model="deepseek-r1-distill-qwen-1.5b",
+#     base_url="http://127.0.0.1:1234/v1",
+#     api_key=SecretStr("lm-studio"),
+# )
 
 # 定义工具
 @tool
@@ -22,8 +33,10 @@ def multiply(a: int, b: int) -> int:
     """Multiplies a and b."""
     return a * b
 
+# 设置搜索工具
+search = TavilySearchResults(max_results=3)
 
-tools = [add, multiply]
+tools = [add, multiply, search]
 
 # 绑定工具
 llm_with_tools = llm.bind_tools(tools)
@@ -31,6 +44,7 @@ llm_with_tools = llm.bind_tools(tools)
 
 # 使用AI
 query = "1024乘1023等于多少？ 10023901234加上123456789等于多少？"
+# query = "搜索一下 北京今天天气怎么样？"
 # 创建消息列表
 messages = [HumanMessage(query)]
 # AI响应
@@ -42,7 +56,7 @@ messages.append(ai_msg)
 
 # 调用工具
 for tool_call in ai_msg.tool_calls:
-    selected_tool = {"add": add, "multiply": multiply}[tool_call["name"].lower()]
+    selected_tool = {"add": add, "multiply": multiply, "tavily_search_results_json": search}[tool_call["name"].lower()]
     tool_msg = selected_tool.invoke(tool_call)
     messages.append(tool_msg)
 
